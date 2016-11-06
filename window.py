390efbf16a8fb1553c153134ba77691ffabe7305
dannyk660 <daking1@gmail.com>
2016-11-05 23:16:00 -0400

    Only check weather API every 15 minutes

--- window.py
+++ window.py
@@ -42,47 +42,45 @@ if not int(settings['auto']):
 		print('Auto brightness disabled, exiting...')
 	exit()
 	
-# Get weather from Yahoo to determine max brightness
-url = "https://query.yahooapis.com/v1/public/yql?q=select item.condition, astronomy.sunrise, astronomy.sunset from weather.forecast where woeid=" + woeid + "&format=json"
 
-try:
-	data = requests.get(url, timeout=10).json()
+url = "https://query.yahooapis.com/v1/public/yql?q="
+url = url + "select item.condition, astronomy.sunrise, astronomy.sunset "
+url = url + "from weather.forecast where woeid=" + woeid + "&format=json"
 
-	weatherCode = int(data['query']['results']['channel']['item']['condition']['code'])
-	weatherText = data['query']['results']['channel']['item']['condition']['text']
-	sunrise = data['query']['results']['channel']['astronomy']['sunrise']
-	sunset = data['query']['results']['channel']['astronomy']['sunset']
+# Refresh weather data every 15 minutes
+if (settings['timestamp'] + 900) < time.time():
+	try:
+		if debug:
+			print('Getting Yahoo! weather data...')
+			
+		data = requests.get(url, timeout=10).json()
 	
-	# Save/cache values
-	settings['auto'] = 1
-	settings['weather'] = weatherCode
-	settings['sunrise'] = sunrise
-	settings['sunset'] = sunset
-	
-	f = open(confFile, 'w')
-	f.write(json.dumps(settings))
-	f.close()
-	
-except:
-	print("Error: Unable to connect to Yahoo! API")
-	
-	# Use settings from cache
-	weatherCode = settings['weather']
-	weatherText = ""
-	sunrise = settings['sunrise']
-	sunset = settings['sunset']
+		# Save/cache values
+		settings['auto'] = 1
+		settings['weather'] = int(data['query']['results']['channel']['item']['condition']['code'])
+		settings['weatherText'] = data['query']['results']['channel']['item']['condition']['text']
+		settings['sunrise'] = data['query']['results']['channel']['astronomy']['sunrise']
+		settings['sunset'] = data['query']['results']['channel']['astronomy']['sunset']
+		settings['timestamp'] = round(time.time())
+		
+		f = open(confFile, 'w')
+		f.write(json.dumps(settings))
+		f.close()
+		
+	except:
+		print("Error: Unable to connect to Yahoo! API")
 
 
 # Set max brightness based on weather
-if weatherCode < 23 or weatherCode in [26,41,42,43]: 
+if settings['weather'] < 23 or settings['weather'] in [26,41,42,43]: 
 	maxBright = cloudy
-elif weatherCode >= 32 and weatherCode <= 36:
+elif settings['weather'] >= 32 and settings['weather'] <= 36:
 	maxBright = sunny
 else:
 	maxBright = mixed
 
 if debug:
-	print("Weather code: " + str(weatherCode) + " (" + weatherText + "), Sunrise: " + sunrise + ", Sunset: " + sunset)
+	print("Weather code: " + str(settings['weather']) + " (" + settings['weatherText'] + "), Sunrise: " + settings['sunrise'] + ", Sunset: " + settings['sunset'])
 	print("Max brightness: " + str(maxBright))
 
 # Current time
@@ -90,13 +88,13 @@ cTime = time.localtime()
 now = time.time()
 
 
-# Sunrise: start brightening 15 mins before, end 75 mins after
-sunriseTime = str(cTime[0]) + '-' + str(cTime[1]) + '-' + str(cTime[2]) + ' ' + sunrise
-sunriseStart = int(time.mktime(time.strptime(sunriseTime, "%Y-%m-%d %I:%M %p"))) - 900
+# Sunrise: start brightening 20 mins before, end 70 mins after
+sunriseTime = str(cTime[0]) + '-' + str(cTime[1]) + '-' + str(cTime[2]) + ' ' + settings['sunrise']
+sunriseStart = int(time.mktime(time.strptime(sunriseTime, "%Y-%m-%d %I:%M %p"))) - 1200
 sunriseEnd = sunriseStart + 5400
 
 # Sunset: start dimming 75 mins before, end 15 mins after
-sunsetTime = str(cTime[0]) + '-' + str(cTime[1]) + '-' + str(cTime[2]) + ' ' + sunset
+sunsetTime = str(cTime[0]) + '-' + str(cTime[1]) + '-' + str(cTime[2]) + ' ' + settings['sunset']
 sunsetStart = int(time.mktime(time.strptime(sunsetTime, "%Y-%m-%d %I:%M %p"))) - 4500
 sunsetEnd = sunsetStart + 5400
 
